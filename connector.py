d0f2de221d79722153bd6f085102f344f9264d49
dimas bagus santoso <santoso.db@gmail.com>
2020-11-06 10:11:34 +0700

    update

--- connector.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from config.config import Config
-
-
-def connect_mongo(): 
-    client = MongoClient(Config.MONGO_HOST)
-    db = client[Config.MONGO_DB]
-    return db
